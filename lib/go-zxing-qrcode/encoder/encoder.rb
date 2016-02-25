97227787116616d8d8e53d1f5bf780d0b4588fa8
hSATAC <hsatac@gmail.com>
2016-02-25 15:48:45 +0800

    Determine which library to load based on RUBY_PLATFORM

--- lib/go-zxing-qrcode/encoder/encoder.rb
+++ lib/go-zxing-qrcode/encoder/encoder.rb
@@ -5,7 +5,11 @@ module GoZXingQRCode
   class Encoder
     include Singleton
     extend FFI::Library
-    ffi_lib File.expand_path("./libgoqrencoder.so", File.dirname(__FILE__))
+    if RUBY_PLATFORM =~ /linux/
+      ffi_lib File.expand_path("./libgoqrencoder_x86_64_linux.so", File.dirname(__FILE__))
+    elsif RUBY_PLATFORM =~ /darwin/
+      ffi_lib File.expand_path("./libgoqrencoder_x86_64_darwin.so", File.dirname(__FILE__))
+    end
     attach_function :generateQRCodeImage, [:string, :string, :int, :int], :void
   end
 end
